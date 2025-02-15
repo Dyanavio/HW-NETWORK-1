c513c04ddfa511eaf6561c9c56af3e36f6b795c2
Dyanavio <dimant2018@gmail.com>
2025-02-15 13:11:33 +0300

    Add project files.

---
+++ HW-NETWORK-1-CLIENT/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace HW_NETWORK_1_CLIENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.5";
+            const int port = 5050;
+            const int fromLength = 4;
+            const int toLength = 3;
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("------ Available convertions ------\n\t-UAH USD\n\t-USD UAH\n\t-USD EUR\n\t-EUR USD\n\t-EUR GBP\n\t-GBP EUR\n\t-SEK UAH\n\t-UAH SEK\n");
+            Console.ResetColor();
+
+            while (true)
+            {
+                Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+                
+                try
+                {
+                    Console.Write("Enter convertion currencies (FROM -> TO)\nFROM('Exit' to exit the program): ");
+                    string? fromCurrency = (Console.ReadLine() + ' ').ToUpper();
+                    Console.Write("TO: ");
+                    string? toCurrency = (Console.ReadLine())?.ToUpper().Trim();
+                    if (fromCurrency.Trim() == "EXIT")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Exiting . . .");
+                        Console.ResetColor();
+                        break;
+                    }
+                    if (string.IsNullOrWhiteSpace(fromCurrency) || string.IsNullOrEmpty(toCurrency))
+                    {
+                        throw new Exception("Currencies cannot be null");
+                    }
+                    if (toCurrency.Length > toLength || fromCurrency.Length > fromLength)
+                    {
+                        throw new Exception("Currency index is invalid");
+                    }
+                    
+                    byte[] fromData = Encoding.UTF8.GetBytes(fromCurrency);
+                    byte[] toData = Encoding.UTF8.GetBytes(toCurrency);
+
+                    client.Connect(endPoint);
+                    client.Send(fromData);
+                    client.Send(toData);
+
+                    byte[] buffer = new byte[1024];
+                    int size = 0;
+                    StringBuilder reply = new StringBuilder();
+
+                    do
+                    {
+                        size = client.Receive(buffer);
+                        reply.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                    }
+                    while (client.Available > 0);
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(reply + "\n");
+                    Console.ReadKey();
+                    Console.ResetColor();
+
+
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine(ex.Message);
+                    Console.ResetColor();
+                }
+            }
+            
+
+        }
+    }
+}
---
+++ HW-NETWORK-1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace HW_NETWORK_1
+{
+    
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, double> converters = new SortedDictionary<string, double>()
+            {
+                ["UAH USD"] = 0.024042,
+                ["USD UAH"] = 41.5943,
+                ["USD EUR"] = 0.953,
+                ["EUR USD"] = 1.0493,
+                ["EUR GBP"] = 0.8337,
+                ["GBP EUR"] = 1.1995,
+                ["SEK UAH"] = 3.889984,
+                ["UAH SEK"] = 0.25707
+            };
+            
+            const string ip = "127.0.0.5";
+            const int port = 5050;
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(endPoint);
+            server.Listen(5);
+            while (true)
+            {
+                string log = "";
+
+                Socket listener = server.Accept();
+
+                log += $"Connection time: {DateTime.Now}\n";
+                byte[] buffer = new byte[1024];
+                int size = 0;
+                StringBuilder data = new StringBuilder();
+
+                do
+                {
+                    size = listener.Receive(buffer);
+                    data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                   // data.Append(' ');
+                } 
+                while (listener.Available > 0);
+                log += $"  -{data}\n";
+                //Console.WriteLine(data.ToString());
+                try
+                {
+                    string? reply = $"{data}\n1 -> {converters[data.ToString()]}";
+                    byte[] buff = Encoding.UTF8.GetBytes(reply);
+
+                    listener.Send(buff);
+
+                }
+                catch(KeyNotFoundException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine(e.Message);
+                    Console.ResetColor();
+                }
+                catch(Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine(e.Message);
+                    Console.ResetColor();
+                }
+
+                log += $"Disconnect time: {DateTime.Now}\n";
+                listener.Shutdown(SocketShutdown.Both);
+                listener.Close();
+
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(log);
+
+            }
+        }
+    }
+}
